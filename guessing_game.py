27abc6cf1e93c2c07795dc397f9fd51391bdff14
dlibberton <drew.libberton@gmail.com>
2024-02-06 15:46:24 -0500

    added class and guess loop

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,40 @@
-class GuessingGame():
+# init with integer
+# GuessingGame#guess
+#
+
+import random
+
+class GuessingGame:
+    def __init__(self, answer, last_guess):
+        self.answer = answer
+        self.last_guess = last_guess
+        self.last_result = None
+
+    def solved(self):
+        return self.last_result == "correct"
+
+    def guess(self, number):
+        number = int(number)#for comparison
+        if number == self.answer:
+            self.last_result = "correct"
+        elif number < self.answer:
+            self.last_result = "too low"
+        else:
+            self.last_result = "too high"
+        self.last_guess = number
+        return self.last_result
+
+# ----- main.py -----from readme
+game = GuessingGame(random.randint(1,100))
+last_guess  = None
+last_result = None
+
+while game.solved() == False:
+    if last_guess != None: 
+        print(f"Oops! Your last guess ({last_guess}) was {last_result}.")
+        print("")
+
+    last_guess  = input("Enter your guess: ")
+    last_result = game.guess(last_guess)
+
+print(f"{last_guess} was correct!")
